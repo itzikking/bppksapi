e588a2bb53d5786fb0752b763b4c3eb344daad27
itzik savaia <44684932+itzikking@users.noreply.github.com>
2019-12-13 14:08:28 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
 // Serve only the static files form the dist directory
@@ -12,7 +11,6 @@ app.get('/*', function(req,res){
     console.log(" Fetching from.." + fullPath);
       res.sendFile(fullPath);
 })
-
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
