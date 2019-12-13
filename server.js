a40e9911a0a49ef7297f258cf55efb23eda30e7b
itzik savaia <44684932+itzikking@users.noreply.github.com>
2019-12-13 13:48:43 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -8,15 +8,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist/bppksapi'));
 
 app.get('/*', function(req,res){
-    const fullPath = path.join(__dirname + '/dist/index.html');
+    const fullPath = path.join(__dirname + '/dist/bppksapi/index.html');
     console.log(" Fetching from.." + fullPath);
       res.sendFile(fullPath);
 })
-  
-// app.get('*', function(req,res) {
-    
-// res.sendFile(path.join(__dirname + '/dist/bppksapi/index.html'));
-// });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
