69777f77a44b0e03f1719d0c4c6fcfa5a7e74b47
itzik savaia <44684932+itzikking@users.noreply.github.com>
2019-12-13 12:21:27 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/booksapi'));
+app.use(express.static(__dirname + '/dist/bppksapi'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/booksapi/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bppksapi/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
